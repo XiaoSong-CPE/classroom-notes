ce02548a98f4db9afeb21c479243c284cf8587a5
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2024-06-03 14:17:57 +0000

    add new notes

--- .vitepress/cache/deps/@theme_index.js
+++ .vitepress/cache/deps/@theme_index.js
@@ -9,32 +9,32 @@ import {
 } from "./chunk-JWA4H7QR.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
-import "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default4 } from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default5 } from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default6 } from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default7 } from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default8 } from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default9 } from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default10 } from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default11 } from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default12 } from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default13 } from "D:/文档/GitHub/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
+import "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default4 } from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default5 } from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default6 } from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default7 } from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default8 } from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default9 } from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default10 } from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default11 } from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default12 } from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default13 } from "/workspaces/classroom-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/vitepress/dist/client/theme-default/support/utils.js
 import { withBase } from "vitepress";
--- .vitepress/config.mts
+++ .vitepress/config.mts
@@ -4,42 +4,76 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "Classroom Notes",
   description: "The lessons I met at Skyedu",
+  lastUpdated: true,
   themeConfig: {
+    search: {
+      provider: 'local'
+    },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: "Home", link: "/" },
       {
         text: "Notes",
         items: [
-          // 25. Mai - 1精学 - Yuki.md
+          // 01 - 25. Mai. - 1精学 - Yuki.md
           {
-            text: "25. Mai - 1精学 - Yuki ⭐",
-            link: "/25. Mai - 1精学 - Yuki",
+            text: "01 - 25. Mai. - 1精学 - Yuki ⭐",
+            link: "/01 - 25. Mai. - 1精学 - Yuki",
           },
-          // 25. Mai - 3精学 - Aven.md
+          // 02 - 25. Mai. - 3素养 - Doris.md
           {
-            text: "25. Mai - 3精学 - Aven",
-            link: "/25. Mai - 3精学 - Aven",
+            text: "02 - 25. Mai. - 3素养 - Doris",
+            link: "/02 - 25. Mai. - 3素养 - Doris",
           },
-          // 25. Mai - 3素养 - Doris.md
+          // 03 - 25. Mai. - 3精学 - Aven.md
           {
-            text: "25. Mai - 3素养 - Doris",
-            link: "/25. Mai - 3素养 - Doris",
+            text: "03 - 25. Mai. - 3精学 - Aven",
+            link: "/03 - 25. Mai. - 3精学 - Aven",
           },
-          // 26. Mai - 3精学 - Charline.md
+          // 04 - 26. Mai. - 3精学 - Doris.md
           {
-            text: "26. Mai - 3精学 - Charline",
-            link: "/26. Mai - 3精学 - Charline",
+            text: "04 - 26. Mai. - 3精学 - Doris",
+            link: "/04 - 26. Mai. - 3精学 - Doris",
           },
-          // 26. Mai - 3精学 - Cindy.md
+          // 05 - 26. Mai. - 3精学 - Cindy.md
           {
-            text: "26. Mai - 3精学 - Cindy",
-            link: "/26. Mai - 3精学 - Cindy",
+            text: "05 - 26. Mai. - 3精学 - Cindy",
+            link: "/05 - 26. Mai. - 3精学 - Cindy",
           },
-          // 26. Mai - 3精学 - Doris.md
+          // 06 - 26. Mai. - 3精学 - Charline.md
           {
-            text: "26. Mai - 3精学 - Doris",
-            link: "/26. Mai - 3精学 - Doris",
+            text: "06 - 26. Mai. - 3精学 - Charline",
+            link: "/06 - 26. Mai. - 3精学 - Charline",
+          },
+          // 07 - 1. Jun. - 3睿学 - Rick.md
+          {
+            text: "07 - 1. Jun. - 3睿学 - Rick",
+            link: "/07 - 1. Jun. - 3睿学 - Rick",
+          },
+          // 08 - 1. Jun. - 1好学 - Miriam.md
+          {
+            text: "08 - 1. Jun. - 1好学 - Miriam ⭐",
+            link: "/08 - 1. Jun. - 1好学 - Miriam",
+          },
+          // 09 - 1. Jun. - 2睿学 - Jackie.md
+          {
+            text: "09 - 1. Jun. - 2睿学 - Jackie",
+            link: "/09 - 1. Jun. - 2睿学 - Jackie",
+          },
+          // 10 - 2. Jun. - 3睿学 - Kat.md
+          {
+            text: "10 - 2. Jun. - 3睿学 - Kat",
+            link: "/10 - 2. Jun. - 3睿学 - Kat"
+          },
+          // 11 - 2. Jun. - 2精学 - Icey.md
+          {
+            text: "11 - 2. Jun. - 2精学 - Icey",
+            link: "/11 - 2. Jun. - 2精学 - Icey"
+          },
+          // 12 - 2. Jun. - 2睿学 - Ariana.md
+          {
+            text: "12 - 2. Jun. - 2睿学 - Ariana",
+            link: "/12 - 2. Jun. - 2睿学 - Ariana"
           },
         ],
       },
